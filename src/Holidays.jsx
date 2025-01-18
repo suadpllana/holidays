ddb21a036d23667940598c2ebf7b132433455fda
oltapllana <oltapllana@gmail.com>
2025-01-18 03:35:38 +0100

    first commit

--- src/Holidays.jsx
+++ src/Holidays.jsx
@@ -2,20 +2,36 @@ import React, { useState, useEffect } from 'react';
 
 const Holidays = () => {
   const [holidayData, setHolidayData] = useState([]);
+  const [countries, setCountries] = useState([])
   const [month, setMonth] = useState("");
+  const [country , setCountry] = useState({
+    code: "XK",
+    countryName: "Kosovo"
+  })
+
   const apiKey = import.meta.env.VITE_API_KEY;
 
-  async function getInfo() {
-    const url = `https://holidayapi.com/v1/holidays?pretty&country=XK&year=2024&key=${apiKey}`;
+
+  async function getAllCountries(){
+    const url = `https://holidayapi.com/v1/countries?key=${apiKey}`;
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data.holidays);
+    setCountries(data.countries)
+   
+  }
+  async function getInfo() {
+    const url = `https://holidayapi.com/v1/holidays?pretty&country=${country.code}&year=2024&key=${apiKey}`;
+    const response = await fetch(url);
+    const data = await response.json();
+  
     setHolidayData(data.holidays);
+  
   }
 
   useEffect(() => {
+    getAllCountries()
     getInfo();
-  }, []);
+  }, [country]);
 
   const getMonthName = (date) => {
     const monthMap = {
@@ -34,10 +50,29 @@ const Holidays = () => {
     };
     return monthMap[date] || "";
   };
+  const handleCountryChange = (selectedCode) => {
+  
+    const selectedCountry = countries.find(country => country.code === selectedCode);
+    console.log(selectedCountry)
+    const countryName = selectedCountry.name;
+      setCountry({
+        code: selectedCode,
+        countryName
+      })
+  };
+  
 
   return (
     <div>
-      <h1>Official Holidays in Kosovo</h1>
+      <p>Get holidays of: </p>
+      <select onChange={(e) => handleCountryChange(e.target.value)}>
+        <option value="">Select a country</option>
+
+        {countries.map(country => (
+          <option key={country.code} value={country.code}>{country.name}</option>
+        ))}
+      </select>
+      <h1>Official Holidays in {country.countryName}</h1>
       <div className="container">
         {holidayData.length > 0 ? (
           <>
